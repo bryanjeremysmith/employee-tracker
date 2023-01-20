ad3adb957342d1032b12e12ab2eb63a17d028514
BJ Smith <bryanjeremysmith@gmail.com>
2023-01-19 21:29:38 -0800

    skeletal code to the main prompt

--- server.js
+++ server.js
@@ -23,4 +23,32 @@ const db = mysql.createConnection(
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
 });
-  
+
+
+const mainPrompt = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What would you like to do?",
+            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+        }
+    ])
+    .then(answer => {
+        if(answer == 'View all Departments'){
+            viewAllDepartments();
+        }else if (answer == 'View all Roles'){
+            viewAllRoles();
+        }else if (answer == 'View all Employees'){
+            viewAllEmployees();
+        }else if (answer == 'Add a Department'){
+            addADepartment();
+        }else if (answer == 'Add a Role'){
+            addARole();
+        }else if (answer == 'Add an Employee'){
+            addAnEmployee();
+        }else if (answer == 'Update an Employee Role'){
+            updateAnEmployeeRole();
+        }
+    })
+};
