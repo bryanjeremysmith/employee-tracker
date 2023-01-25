a8693142f4830f548a63b7216ab9bee2a363c27d
BJ Smith <bryanjeremysmith@gmail.com>
2023-01-25 13:57:42 -0800

    adding schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCRMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT FOREIGN KEY (department_id) REFERENCES department(id)
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(15),
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
+
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INT
-)
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT REFERENCES employee(id)
+);
